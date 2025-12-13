4dc21b182b757dc6a274f065dd3330e42819f730
sckratcher <sckratch0110@gmail.com>
2025-12-13 13:50:50 +0100

    reedition

---
+++ src/core/include/Core/Core.hpp
@@ -0,0 +1,10 @@
+#ifndef _CORE_CORE_HPP__
+#define _CORE_CORE_HPP__
+
+
+#include "Core/Application/Application.hpp"
+#include "Core/Log/Log.hpp"
+#include "Core/Utils/ServiceLocator.hpp"
+
+
+#endif 
